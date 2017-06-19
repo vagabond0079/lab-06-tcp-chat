e7abdfeda982782674602e3cb00188a2f84982f6
derekcfarmer <derek.farmer@gmail.com>
2017-06-19 15:56:59 -0700

    working on /dm.

--- lab-derek/index.js
+++ lab-derek/index.js
@@ -12,8 +12,8 @@ let curUser = 1;
 //TODO: When sockets connect to the server a new Client instance should be made
 
 server.on('connection', (socket) =>{
-  console.log('socket connected.');
-  socket.write('Hey there, friend. Welcome to HooliChat.');
+  console.log('socket connected.\n');
+  socket.write('Hey there, friend. Welcome to HooliChat.\n');
 
 //TODO: Clients should have a unique 'nickname'
 //TODO: e.g. guest-43
@@ -21,9 +21,14 @@ server.on('connection', (socket) =>{
   socket.nickname = `guest-${curUser}`;
   curUser++;
   clientPool = [...clientPool, socket];
+  socket.write(`You\'re connected as ${socket.nickname}\n`);
+
+
+//TODO: When a socket emits the close event the socket should be removed from the client pool!
+//TODO: When a socket emits the error event the error should be logged on the server
 
   let handleDisconnect = () => {
-    console.log(`${socket.nickname} has disconnected`);
+    console.log(`${socket.nickname} has disconnected\n`);
     clientPool = clientPool.filter(item => item !== socket);
   }
 
@@ -32,24 +37,58 @@ server.on('connection', (socket) =>{
   socket.on('error', handleDisconnect);
   socket.on('close', handleDisconnect);
 
+//TODO: When a socket emits the data event the data should be logged on the server and the \wack commands below should be implemented
+
   socket.on('data', (buffer) => {
     let data = buffer.toString();
-    if (data.startsWith('/nickname')){
-      socket.nickname = data.split('/nickname')[1] || socket.nickname;
+    let content = buffer.toString();
+    let print = (users) => {
+      clientPool.forEach((user) => {
+          user.write(`${socket.nickname}: ${content.toString()}`)
+      })
+    };
+
+//TODO: /nick should allow a user change their nickname
+
+    if (data.startsWith('/nick')){
+      socket.nickname = data.split('/nick')[1] || socket.nickname;
       socket.nickname = socket.nickname.trim();
-      socket.write(`you are now known as ${socket.nickname}`);
+      socket.write(`you are now known as ${socket.nickname}\n`);
       return
+    }
+
+
+//TODO: /dm should allow a user to send a message directly to another user by nick name
+
+    if (data.startsWith('/dm')){
+      socket.write(`testing ${data}\n`);
+      let wholeMsg = data.split('/dm ')[1] || "";
+      socket.write(`wholeMsg: ${wholeMsg}\n`)
+      let dmRecipient = wholeMsg.split(/\s+/)[0];
+      let content = wholeMsg.replace(dmRecipient, '');
+      socket.write(`content: ${content}\n`);
+      socket.write(`dmRecipient: ${dmRecipient}\n`);
+      // clientPool.forEach((user) => {
+      //   if (user = dmRecipient){
+      //     user.write(`to ${dmRecipient}: ${content.split(/\s+/)[1]}`)
+      //   }
+      // })
+    }
+
+//TODO: when a user speaks their nickname should be printed, i.e. teapot: Sup Hacker?
+
+    else{
+      print(clientPool);
     }
   })
 })
 
+//TODO: /troll should take in a number and a message and send the message to everyone that number of times
 
 
+//TODO: /quit should close the connection with the user
 
 
-
-
-
-//TODO: When a socket emits the close event the socket should be removed from the client pool!
-//TODO: When a socket emits the error event the error should be logged on the server
-//TODO: When a socket emits the data event the data should be logged on the server and the \wack commands below should be implemented
+server.listen(3000, () => {
+  console.log('server up on port 3000');
+});
